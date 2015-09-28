84f4388608ef456a4f3650de1f556236034a263d
Matteo Manica <matteo.manica@moxoff.com>
2015-09-28 15:03:51 +0200

    Edited .gitignore for nbproject

--- FunctionalUtils.hpp
+++ FunctionalUtils.hpp
@@ -11,26 +11,57 @@
 #include<algorithm>
 #include<functional>
 #include<utility>
+#include<map>
+#include<set>
 
 class FunctionalUtils {
 public:
 
+    template <typename Container>
+    static void mkString(const Container & container, const char* separator = " ", std::ostream & out = std::cout) {
+        std::for_each(container.begin(), container.end() - 1, [&separator, &out]
+                (const typename Container::value_type & element) {
+            out << element << separator;
+        });
+        out << container.back() << std::endl;
+    }
+
+    //template parameters
+
     template < template <typename, typename> class Container,
     typename IType, typename IAllocator = std::allocator<IType>,
     typename OType, typename OAllocator = std::allocator<OType> >
+    //declaration
     static Container<OType, OAllocator> map(const Container<IType, IAllocator> & input, const std::function<OType(IType)> & func) {
+        //body
         Container<OType, OAllocator> output(input.size());
         std::transform(input.begin(), input.end(), output.begin(), func);
         return output;
     }
 
+    //template parameters
+
+    template < class Container>
+    //declaration
+    static Container filter(const Container & input,
+            const std::function<bool(typename Container::value_type)> & func) {
+        //body
+        Container output;
+        typename Container::const_iterator filtered = std::copy_if(input.begin(), input.end(), output.begin(), func);
+        output.resize(std::distance(output.begin(), filtered));
+        return output;
+    }
+
+    //template parameters
+
     template < template <typename, typename> class Container,
     typename FirstType, typename FirstAllocator = std::allocator<FirstType>,
     typename SecondType, typename SecondAllocator = std::allocator<SecondType>,
     typename OType = std::pair<FirstType, SecondType>, typename OAllocator = std::allocator<OType> >
-    static Container< OType, OAllocator > zip(const Container<FirstType, FirstAllocator> & aContainer, 
-    const Container<SecondType, SecondAllocator> & anotherContainer) {
-
+    //declaration
+    static Container< OType, OAllocator > zip(const Container<FirstType, FirstAllocator> & aContainer,
+            const Container<SecondType, SecondAllocator> & anotherContainer) {
+        //body
         const unsigned long int & dimension = std::min(aContainer.size(), anotherContainer.size());
         typename Container<FirstType, FirstAllocator>::const_iterator aIt = aContainer.begin();
         typename Container<SecondType, SecondAllocator>::const_iterator anotherIt = anotherContainer.begin();
@@ -44,6 +75,58 @@ public:
         return output;
     }
 
+    //template parameters
+
+    template < template <typename, typename> class Container,
+    typename IType, typename IAllocator = std::allocator<IType>,
+    typename Key, typename Comparator = std::less<Key>,
+    typename KIAllocator = std::allocator< std::pair<const Key, IType> > >
+    //declaration
+    static std::multimap<Key, IType, Comparator, KIAllocator> keyBy(const Container<IType, IAllocator> & input,
+            const std::function< Key(IType)> & func) {
+        //body
+        std::multimap<Key, IType, Comparator, KIAllocator> output;
+        std::transform(input.begin(), input.end(), std::inserter(output, output.end()),
+                [&func](const IType & element) {
+                    return std::make_pair(func(element), element);
+                }
+        );
+        return output;
+    }
+
+    //template parameters
+
+    template < template <typename, typename> class Container,
+    typename IType, typename IAllocator = std::allocator<IType>,
+    typename Key, typename Comparator = std::less<Key>,
+    typename Value = std::vector<IType>,
+    typename KIAllocator = std::allocator< std::pair<const Key, IType> >,
+    typename KIType = std::pair<const Key, IType>,
+    typename KVAllocator = std::allocator< std::pair<const Key, Value> > >
+    //declaration
+    static std::map<Key, Value, Comparator, KVAllocator> groupBy(const Container<IType, IAllocator> & input,
+            const std::function< Key(IType)> & func) {
+        //body
+        std::map<Key, Value, Comparator, KVAllocator> output;
+                
+        const std::multimap<Key, IType, Comparator, KIAllocator>  & mapped = keyBy(input, func);
+        std::set<Key> keys;
+        std::transform(mapped.begin(), mapped.end(), std::inserter(keys, keys.begin()), 
+                []( const KIType & keyValue ) { return keyValue.first;});
+        
+        typedef typename std::multimap<Key, IType, Comparator, KIAllocator>::const_iterator multiConstIt;
+        
+        for( const Key & key : keys ){
+            std::pair<multiConstIt, multiConstIt> values= mapped.equal_range(key);
+            std::vector<IType> grouped;
+            grouped.reserve( std::distance( values.first, values.second) );
+            for(multiConstIt it= values.first; it!=values.second; ++it){
+                grouped.push_back(it->second);
+            }
+            output.emplace(key, grouped);            
+        }
+        return output;
+    }
 
 
 };
--- main.cpp
+++ main.cpp
@@ -1,38 +1,71 @@
 #include<iostream>
 #include<vector>
+#include<math.h>
 #include "FunctionalUtils.hpp"
+
+#include<cassert>
 
 using namespace std;
 
+int main(){
 
-template <typename C>
-static void printContainer(const C & container, const char* separator=" ", std::ostream & out=std::cout) {
-  std::for_each(container.begin(), container.end()-1, [&separator, &out](const typename C::value_type & element) {
-      out << element << separator;
-      });
-  out << container.back() << std::endl;
-}
+    
+  
+  vector<int> test1={1, 3, 34, 50};
+  vector<double> test2={1.0, 2.0, 3.0};
 
-
-
-int main( int argc, char** argv){
-
-  vector<int> test={1, 3, 34, 50};
-  vector<double> testBis={1.0, 2.0, 3.0};
-
-  printContainer(test);
-
-  function< double(int) > func= [] (const int & element){return 2.0*element;};
-
-  vector<double> output= FunctionalUtils::map( test,  func );
-
-  printContainer(output,"#",std::cout);
-
-  auto outputZip = FunctionalUtils::zip(test, testBis);
-
+  
+  cout << "Test map..." << endl;
+  vector<double> expectedMap={2, 6, 68, 100};
+  function< double(int) > func1= [] (const int & element){return 2.0*element;};   
+  vector<double> output= FunctionalUtils::map( test1,  func1 );
+  FunctionalUtils::mkString(output);
+  assert(expectedMap==output);
+  cout << "passed!" << endl;
+  
+  cout << "Test zip..." << endl;
+  vector< std::pair<int,double> > expectedZip={
+      std::make_pair(1,1.0),
+      std::make_pair(3,2.0),
+      std::make_pair(34, 3.0)      
+  };
+  auto outputZip = FunctionalUtils::zip(test1, test2);  
   for( const auto & couple : outputZip){
     std::cout << couple.first << " "<< couple.second << std::endl;
   }
+  assert( expectedZip==outputZip);
+  cout << "passed!" << endl;
+  
+  vector<double> test3={1.0, 1.2, 2.0, 2.3, 3.0};
+  function< int(double) > func2= [] (const double & element){return (int)element;};
+  multimap<int, double> expectedKeyBy={
+      std::make_pair(1,1.0),
+      std::make_pair(1,1.2),
+      std::make_pair(2,2.0),
+      std::make_pair(2,2.3),
+      std::make_pair(3, 3.0)      
+  };
+  cout << "Test keyBy..." << endl;
+  auto keyed = FunctionalUtils::keyBy(test3,func2); 
+  for( const auto & d : keyed){
+      cout << d.first << " -> " << d.second << endl;
+  }
+  assert( expectedKeyBy==keyed);
+  cout << "passed!" << endl;
+  
+  cout << "Test groupBy..." << endl;
+  map<int, vector<double> > expectedGroupBy={
+      std::make_pair(1, std::vector<double>{1.0, 1.2}),      
+      std::make_pair(2, std::vector<double>{2.0, 2.3}),      
+      std::make_pair(3, std::vector<double>{3.0})      
+  };
+  auto grouped = FunctionalUtils::groupBy(test3, func2);  
+  for ( const auto & d : grouped){
+      cout << d.first << " -> ";      
+      FunctionalUtils::mkString(d.second, ",");
+  }
+  assert(expectedGroupBy==grouped);
+  cout << "passed!" << endl;
 
   return 0;
 
